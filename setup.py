34cbfa3b15cbeccf2337f10d748f5f4513c0a4c1
Hedgehog <git@github.com:HedgehogIvan/Figure.git>
2024-03-29 20:57:51 +0300

    Добавлен файл '.gitignore' Добавлена настройка для оборачивания программы в библиотеку

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Shapes',
+    packages=find_packages(include=['shape', 'shape.convex_polygon']),
+    version='0.1.0',
+    description='My_Exclusive_New_Shapes',
+    author='Hed'
+)
