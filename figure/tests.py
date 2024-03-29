285d09e93678d7f251202f8cabd8fafa0ab8bae8
Hedgehog <git@github.com:HedgehogIvan/Figure.git>
2024-03-29 14:10:30 +0300

    Добавлены новые тесты для треугольника. Тесты на площадь и периметр для трёх треугольников

--- figure/tests.py
+++ figure/tests.py
@@ -31,6 +31,9 @@ class TestTriangle(unittest.TestCase):
     def setUp(self):
         self.t_int = Triangle(9, 12, 15)
         self.t_float = Triangle(3.5, 4.6, 7.9)
+        self.t_1 = Triangle(60, 60, 60)
+        self.t_2 = Triangle(5, 10, 7.3)
+        self.t_3 = Triangle(7, 7, 13.53)
 
     def test_area_int(self):
         self.assertEqual(self.t_int.area, 54)
@@ -68,6 +71,23 @@ class TestTriangle(unittest.TestCase):
     def test_catch_sum_eq_error(self):
         self.assertRaises(ValueError, Triangle, 9, 12, 21)
 
+    def test_area_t_1(self):
+        self.assertAlmostEqual(self.t_1.area, 1558.8, 1)
+
+    def test_perimeter_t_1(self):
+        self.assertEqual(self.t_1.perimeter, 180)
+
+    def test_area_t_2(self):
+        self.assertAlmostEqual(self.t_2.area, 17.42, 2)
+
+    def test_perimeter_t_2(self):
+        self.assertEqual(self.t_2.perimeter, 22.3)
+
+    def test_area_t_3(self):
+        self.assertAlmostEqual(self.t_3.area, 12.167, 3)
+
+    def test_perimeter_t_3(self):
+        self.assertEqual(self.t_3.perimeter, 27.53)
 
 if __name__ == '__main__':
     unittest.main()
